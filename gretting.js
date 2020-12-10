9ae4f8ba7b144dbc034df0014cde60561d62f477
glorroll <jkbae09@gmail.com>
2020-12-11 03:13:30 +0900

    Saving the userName
    how to use local storage

--- gretting.js
+++ gretting.js
@@ -1,5 +1,24 @@
 const form = document.querySelector(".js-form");
 input = form.querySelector("input");
+greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser";
+SHOWING_CN = "showing";
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    // 유저가 없는 경우
+  } else {
+    paintGreeting(currentUser);
+    // 유저가 있는 경우
+  }
+}
 function init() {}
 
 init();
